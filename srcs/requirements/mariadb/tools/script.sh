5568c110872b786f230ea6ede5d53ef7f6b82577
Dmitrii Larionov <mikrolux@gmail.com>
2024-03-24 14:21:33 +0700

    up

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,15 +4,16 @@ service mysql start
 
 mysqld --general-log=1 --general-log-file=/var/log/mysql/mysql.log
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+DB_EXISTS=$(mysql -e "SHOW DATABASES LIKE '$db_name';" | grep "$db_name" > /dev/null; echo "$?")
+USER_EXISTS=$(mysql -e "SELECT User FROM mysql.user WHERE User='$db_user';" | grep "$db_user" > /dev/null; echo "$?")
 
-mysql < db1.sql
-
-mysql -u $db_user -p$db_pwd $db_name < /dump.sql
+if [ "$DB_EXISTS" -eq 1 ]; then
+    echo "CREATE DATABASE $db_name ;" > db1.sql
+    echo "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
+    echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
+	mysql < db1.sql
+	mysql -u $db_user -p$db_pwd $db_name < /dump.sql
+fi
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
